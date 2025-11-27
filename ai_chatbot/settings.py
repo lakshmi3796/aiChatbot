b64772f047e38c2e38e00d229879c2026123f386
Rohit Patel <luckykumarpatel2001@gmail.com>
2025-11-27 17:19:13 +0530

    create bot

--- ai_chatbot/settings.py
+++ ai_chatbot/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bypuairx*p+5_^n5mnhqt@chb=j4!!e*yeo(3ux0hevy(v_(b8'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
